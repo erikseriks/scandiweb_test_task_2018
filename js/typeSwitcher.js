b111e05321a3a4de8bf35f89e8229d849b4846e3
Eriks <eriks.kirsis@gmail.com>
2018-02-14 20:26:17 +0200

    .

--- js/typeSwitcher.js
+++ js/typeSwitcher.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     //Show first/default attribute.
-    showSelectedAttribute($("#typeSwitcher").val());
+    showAttribute($("#typeSwitcher").val());
     
     //Show selected attribute.
     $("#typeSwitcher").on( 'click', function () {
-        showSelectedAttribute($(this).val());
+        showAttribute($(this).val());
     });
 
     //Function for displaying only one attribute, that matches typeSwitcher value.
-    function showSelectedAttribute(value){
+    function showAttribute(value){
         var typeSwitcher = $("#typeSwitcher");
 
         for (var i = 0; i < typeSwitcher.length; i++) {
